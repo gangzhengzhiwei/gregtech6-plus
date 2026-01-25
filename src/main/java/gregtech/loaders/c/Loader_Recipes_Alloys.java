bff24ac1b399e5795ed9b5efe6ad49f7bcacbc13
gangzhengzhiwei <mczjxy@163.com>
2026-01-25 12:50:59 +0800

    del old recipes annealed copper

--- src/main/java/gregtech/loaders/c/Loader_Recipes_Alloys.java
+++ src/main/java/gregtech/loaders/c/Loader_Recipes_Alloys.java
@@ -30,8 +30,7 @@ import static gregapi.data.CS.ZL_IS;
 
 public class Loader_Recipes_Alloys implements Runnable {
 	@Override public void run() {
-		
-		RM.Smelter.addRecipe0(T, 16, 16, FL.make("molten.copper", 1), FL.make("molten.annealedcopper", 1), ZL_IS);
+
 		RM.Smelter.addRecipe0(T, 16, 16, FL.make("molten.iron"  , 1), FL.make("molten.wroughtiron"   , 1), ZL_IS);
 		
 		for (String tCopper : new String[] {"molten.copper", "molten.annealedcopper"}) {
